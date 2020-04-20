9d2418ed14b9c4321df0c317815a9a35e95f413c
Zhaoyang Zeng <664587718@qq.com>
2020-04-20 03:27:47 +0000

    update laji

--- data/download.sh
+++ data/download.sh
@@ -1,6 +1,7 @@
 #https://drive.google.com/file/d/1006g9GcixffAisW1KOHEx-2vHPaZHoH6/view?usp=sharing
-filename="image.zip"
-file_id="1006g9GcixffAisW1KOHEx-2vHPaZHoH6"
+#https://drive.google.com/file/d/16BDmHoa1zF9kATirUq0RcwAnKDtJjslk/view?usp=sharing
+filename="laji.zip"
+file_id="16BDmHoa1zF9kATirUq0RcwAnKDtJjslk"
 query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${file_id}" \
 | perl -nE'say/uc-download-link.*? href="(.*?)\">/' \
 | sed -e 's/amp;//g' | sed -n 2p`
