62fcc7a035fafe1f637a4d5c40a8b856f0f00d68
Zhaoyang Zeng <664587718@qq.com>
2020-05-05 07:31:05 +0000

    update config

--- configs/laji/cascade_rcnn_x101_fpn_dcn_ms_1x.py
+++ configs/laji/cascade_rcnn_x101_fpn_dcn_ms_1x.py
@@ -220,7 +220,7 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
     step=[8, 11])
-checkpoint_config = dict(interval=1)
+checkpoint_config = dict(interval=1, create_symlink=False)
 # yapf:disable
 log_config = dict(
     interval=50,
